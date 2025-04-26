0d027d52a00f9d953c8a76cb275b3cde19efdfaa
guilherme <Guilhermepazetti@gmail.com>
2025-04-26 11:47:55 -0300

    bleble

--- api/config/db.js
+++ api/config/db.js
@@ -2,7 +2,7 @@ import { MongoClient } from "mongodb"
 let db;
 export async function connectToDatabase(app){
     try{
-        const MONGODB_URI = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/Barbearia"
+        const MONGODB_URI = process.env.MONGODB_URI || "mongodb+srv://Pazetti:vxDjuUsOo9Bnu45F@barbearia.ckw5xnq.mongodb.net/"
         const client = new MongoClient(MONGODB_URI)
         await client.connect()
         console.log('Conectado ao MongoDB!')
